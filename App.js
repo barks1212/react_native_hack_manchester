1980cbbd7158b243f2861e3fe832ca3955ca5e92
barks1212 <willbarkerwork@gmail.com>
2019-10-26 15:03:20 +0100

    package install and app render

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Container, Text } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+
+const fetchFonts = () =>
+  Font.loadAsync({
+    Roboto: require("native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    ...Ionicons.font
+  });
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading startAsync={fetchFonts} onFinish={() => setIsReady(true)} />
+    );
+  }
+
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
