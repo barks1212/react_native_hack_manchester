f171b18c78fb482238f1993e116c5ef50efa026c
barks1212 <willbarkerwork@gmail.com>
2019-10-27 08:31:38 +0000

    wip

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Button,
+  Text,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform
+} from "react-native";
 
 const HomeScreen = props => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.logoContainer}>
         <Text style={styles.logo}>SUPERMARKET SWEEP</Text>
       </View>
       <View style={styles.menuContainer}>
-        <Button
-          title="New Game"
+        <TouchableOpacity
+          style={styles.customButton}
           onPress={() => {
             props.navigation.navigate("QuizTimer");
           }}
-        />
+        >
+          <Text style={styles.buttonText}>NEW GAME</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -27,20 +43,32 @@ const styles = StyleSheet.create({
   logoContainer: {
     flex: 1,
     marginVertical: 200,
-    width: "100%",
+    width: "90%",
     alignItems: "center"
   },
   logo: {
-    fontFamily: "Roboto-bold",
-    fontSize: 22
+    fontFamily: "Caviar-bold",
+    textAlign: "center",
+    fontSize: 40
   },
   menuContainer: {
     flex: 1,
-    width: "100%"
+    width: "100%",
+    alignItems: "center"
+  },
+  customButton: {
+    width: "50%",
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green",
+    padding: 10
   },
   buttonText: {
-    fontFamily: "Roboto-bold",
-    fontSize: 18
+    fontFamily: "Caviar-bold",
+    fontSize: 30
   }
 });
 
